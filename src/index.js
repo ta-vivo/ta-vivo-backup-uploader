1fb5596efc4012cb5b242178cd24b7b95b0145cb
rafieltq <taverasrafiel@gmail.com>
2023-08-31 17:20:47 -0400

    Added supabase bucket name as an env variable

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ const { createClient } = require('@supabase/supabase-js')
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabaseBucket = process.env.SUPABASE_BUCKET
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 const uploadFile = async () => {
   // generate a file name with current date
-  const fileName = `${new Date().toISOString()}.sql`
-  const buf = fs.readFileSync(__dirname + '/file/dump.sql');
+  const fileName = `${new Date().toISOString()}.tar`
+  const buf = fs.readFileSync(__dirname + '/file/dump.sql.tar.gz');
 
   const { data, error } = await supabase.storage
-    .from('tavivo-backup')
+    .from(supabaseBucket)
     .upload(fileName, buf)
 
   console.log('🚀 ~ file: index.js ~ line 13 ~ uploadFile ~ error', error)
